d8624987b7f0637ea3c71f328c3df507cc0f6a15
Abdallah Essam Shaban <ashaban@saudivts.com>
2024-11-29 13:37:43 +0300

    first commit

---
+++ TasheerEnrollment/TasheerEnrollment.h
@@ -0,0 +1,18 @@
+//
+//  TasheerEnrollment.h
+//  TasheerEnrollment
+//
+//  Created by Abdallah Essam Shaban on 28/11/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TasheerEnrollment.
+FOUNDATION_EXPORT double TasheerEnrollmentVersionNumber;
+
+//! Project version string for TasheerEnrollment.
+FOUNDATION_EXPORT const unsigned char TasheerEnrollmentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TasheerEnrollment/PublicHeader.h>
+
+
